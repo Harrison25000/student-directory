aa66db84e24fc4ff0785b779d01fd6bbf812a145
Oliver <ofcharrison@gmail.com>
2019-01-30 17:49:58 +0000

    Saving data to a file

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+@students = [] #An empty array accessible to all methods
 #putting all students into an array
 def input_students
   puts "Please enter the names of the students"
@@ -10,59 +11,82 @@ def input_students
   while !name.empty? do
     #add the student hash to the array
 
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
+
 #Then we print them
 def print_header
 puts "The students of Villains Academy"
 puts "------------"
 end
-def print(students)
-  if students == []
+
+def print_students_list
+  if @students == []
     puts "We have no students"
     return
   end
-students.each do |student|
+@students.each do |student|
  puts "#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
+
 #We print the total number of students
-def print_footer(names)
-  if names.count == 0
+def print_footer
+  if @students.count == 0
     return
   end
-  puts "Overall, we have #{names.count} great students."
+  puts "Overall, we have #{@students.count} great students."
 end
 
 def interactive_menu
-  students = []
   loop do
-    #1. Print the menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    #2. Read the input and save it into a variable
-    selection = gets.chomp
-    #3. Do what the user has asked
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def process(selection)
+  case selection
     when "1"
       students = input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
       puts "I don't know what you meant, try again"
   end
 end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. save the list to students.csv"
+  puts "9. Exit"
 end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+
 #nothing happens until we have called each method
 interactive_menu
