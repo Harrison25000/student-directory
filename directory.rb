ec56855f55a33a71e4555b33be08f9cb6dcc17c8
Oliver <ofcharrison@gmail.com>
2019-01-30 16:46:34 +0000

    interactive menu

--- directory.rb
+++ directory.rb
@@ -9,6 +9,7 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
+
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     #get another name from user
@@ -23,16 +24,45 @@ puts "The students of Villains Academy"
 puts "------------"
 end
 def print(students)
+  if students == []
+    puts "We have no students"
+    return
+  end
 students.each do |student|
  puts "#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 #We print the total number of students
 def print_footer(names)
+  if names.count == 0
+    return
+  end
   puts "Overall, we have #{names.count} great students."
 end
+
+def interactive_menu
+  students = []
+  loop do
+    #1. Print the menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    #2. Read the input and save it into a variable
+    selection = gets.chomp
+    #3. Do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+end
 #nothing happens until we have called each method
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
